c0e09b97145b44e5151c79c1133bc76d7365051a
choish19 <100904838+choish19@users.noreply.github.com>
2024-05-19 01:21:37 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const http = require('http');
 
-const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -17,6 +16,4 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(port);
